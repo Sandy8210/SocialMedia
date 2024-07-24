0126ea19349c01abcdf0d7d7397b3096661f722e
Sandy8210 <santhoshsvg2410@gmail.com>
2024-07-24 08:18:48 +0530

    top destination complete

--- js/swipper.js
+++ js/swipper.js
@@ -17,3 +17,34 @@ const swiper = new Swiper(".swiper", {
   speed: 1000,
   spaceBetween: 100,
 });
+
+const swiper2 = new Swiper(".swiper2", {
+  slidesPerView: 3,
+  spaceBetween: 35,
+  slidesPerGroup: 1,
+  loop: true,
+  fade: true,
+  centerSlide: true,
+  grabCursor: true,
+  loopfillGroupwithBlank: true,
+  autoplay: {
+    delay: 500,
+  },
+  speed: 1000,
+  breakpoints: {
+    // whwn window width is >= 320px
+    320: {
+      slidesPerView: 1,
+    },
+
+    // whwn window width is >= 768px
+    768: {
+      slidesPerView: 2,
+    },
+
+    // whwn window width is >= 968px
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
